43bf33068c0ce5271836f5d02faa776373690c6f
Kevin Tsai <k.tsai92@gmail.com>
2021-05-05 22:57:38 -0400

    added constructor

--- src/App.js
+++ src/App.js
@@ -1,43 +1,51 @@
 import './App.css';
 import igor from './pictures/igor.jpg'
 
-function App() {
-  return (
-    <div className = "player">
-      <section className = "details">
-        <div className = "now-playing">
-          <h3>Playing X of Y</h3>
-        </div>
-        <div className="track-art">
-          <img src = {igor} alt = "album-cover" className="album-art"/>
-        </div>
-        <div className="track-name">Track Name - Track Artist</div>
-      </section>
-      <section className = "buttons">
-        <div className= "previous-track">
-          <i className ="fa fa-step-backward fa-2x"></i>
-        </div>
-        <div className = "play-button">
-          <i className ="fa fa-play-circle fa-5x"></i>
-        </div>
-        <div className = "next-track">
-          <i className = "fa fa-step-forward fa-2x"></i>
-        </div>
-      </section>
-      <section className = "slider_container">
-        <div className = "current-time">00:00</div>
-        <input type="range" min="1" max = "100"
-        value = "0" className="seek_slider"/>
-        <div className = "total-duration">00:00</div>
-      </section>
-      <section className="slider_container">
-        <i className="fa fa-volume-down"></i>
-        <input type="range" min="1" max="100"
-          value="99" className="volume_slider"/>
-        <i className="fa fa-volume-up"></i>
-      </section>
-  </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state= {
+
+    }
+  }
+  render() {
+    return (
+      <div className = "player">
+        <section className = "details">
+          <div className = "now-playing">
+            <h3>Playing X of Y</h3>
+          </div>
+          <div className="track-art">
+            <img src = {igor} alt = "album-cover" className="album-art"/>
+          </div>
+          <div className="track-name">Track Name - Track Artist</div>
+        </section>
+        <section className = "buttons">
+          <div className= "previous-track">
+            <i className ="fa fa-step-backward fa-2x"></i>
+          </div>
+          <div className = "play-button">
+            <i className ="fa fa-play-circle fa-5x"></i>
+          </div>
+          <div className = "next-track">
+            <i className = "fa fa-step-forward fa-2x"></i>
+          </div>
+        </section>
+        <section className = "slider_container">
+          <div className = "current-time">00:00</div>
+          <input type="range" min="1" max = "100"
+          value = "0" className="seek_slider"/>
+          <div className = "total-duration">00:00</div>
+        </section>
+        <section className="slider_container">
+          <i className="fa fa-volume-down"></i>
+          <input type="range" min="1" max="100"
+            value="99" className="volume_slider"/>
+          <i className="fa fa-volume-up"></i>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
