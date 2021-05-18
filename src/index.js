19e08ff634126c3c0d710cbcbb553078074b69a0
Kevin Tsai <k.tsai92@gmail.com>
2021-05-18 15:38:21 -0400

    changed project to include React Hooks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AudioPlayer from './AudioPlayer';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AudioPlayer />
   </React.StrictMode>,
   document.getElementById('root')
 );
